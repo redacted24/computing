72cc0cd6d4ec72d81ccf2eb20ad6c8a88c3fbeca
redacted24 <huangq4@outlook.com>
2023-11-09 22:10:04 -0500

    sync

---
+++ 4A.py
@@ -0,0 +1,14 @@
+# https://codeforces.com/problemset/problem/4/A
+# Watermelon
+
+w = int(input())
+
+if w == 2:
+    print("NO")
+
+elif w % 2 == 0:
+    print("YES")
+else:
+    print("NO")
+
+    # Case of 2 watermelon weight! Careful
