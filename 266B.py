2d51c6e6d0dbc4e172b7df7f3186aba08752b160
redacted24 <huangq4@outlook.com>
2024-01-18 11:10:33 -0500

    sync

--- 266B.py
+++ 266B.py
@@ -1,19 +1,26 @@
 # Queue at school
 # https://codeforces.com/problemset/problem/266/B
 
-t = [int(x) for x in input().split(' ')]
-queue = list(input())
-time = t[1]
-counter = 0
+x = [int(x) for x in input().split()]
+line = str(input())
+temp = ''
+seconds = x[1]
+people = x[0]
+i = 0
 
-while counter < len(queue):
-  if queue[counter] == 'B':
-    index = queue.index('B')
-    queue.pop(index)
-    queue.insert(index+1, 'B')
-    counter += 1
-  counter += 1
-    
+for j in range(seconds):
+  print("Running loop")
+  while i < people:
+    if line[i+1] == 'G' and line[i] == 'B':
+      temp += 'GB'
+      i += 2
+    else:
+      temp += line[i]
+      i += 1
+      print(temp, i)
 
+  # reset
+  line = temp
+  i = 0
 
-print(queue)
+print(temp)
