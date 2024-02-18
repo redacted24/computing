bca8893baf75301d9ccd248445ca4e51c2d0b8c5
redacted24 <huangq4@outlook.com>
2024-02-18 18:56:19 -0500

    trying to figure out how to parse all elements of matirx into diagonals

--- CCC/CCC2023jun/CCCwordhunt.py
+++ CCC/CCC2023jun/CCCwordhunt.py
@@ -1,28 +1,66 @@
 class Puzzle:
     def __init__(self):
-        a = input()
-        self.word = [a, a[::-1]]
+        self.word = input()
         self.height = int(input())
         self.table = []
-        input()
+        self.count = 0
+        self.width = int(input())
         for i in range(self.height):
             content = ''.join(input().split(' '))
             self.table.extend([content])
+    
+    def __repr__(self):
+        '''Prints the number of words found'''
+        return f'{str(self.count)} words found: {str(self.word)} table: {self.table}'
+
+    def wordFinder(self, table_line):
+        '''Main func to iterate through strings and find how many occurences of the substring appears'''
+        self.count += table_line.count(self.word)
+    
+    def straight(self, table):
+        '''Iterate through a table, straight forward direction'''
+        for line in table:
+            self.wordFinder(line)
         
-        print(self.table)
+    def inverse(self, table):
+        '''Iterate through a table, reverse direction'''
+        for line in table:
+            self.wordFinder(line[::-1])
+    
+    def searchHorizontal(self):
+        '''Search for horizontal'''
+        self.straight(self.table)
+        self.inverse(self.table)
+    
+    def searchVertical(self):
+        '''Search for Vertical'''
+        temp = []
+        for j in range(self.width):
+            tempstr = ''
+            for i in range(self.height):
+                tempstr += self.table[i][j]
+            temp.append(tempstr)
+        self.straight(temp)
+        self.inverse(temp)
+            
+    def searchDiagonal(self):
+        '''Search for Diagonals'''
+        diag = [[] for _ in range(self.width + self.height -1)]
+        bdiag = [[] for _ in range(len(diag))]
+        min_diag = -self.width+1
+        for x in range(self.height):
+            for y in range(self.width):
+                diag[x+y].append(self.table[x][y])
+                bdiag[x-y-min_diag].append(self.table[y][x])
 
-    def checker(self, index, letter):
-        if letter == self.word[0][index] or letter == self.word[1][index]:
-            return True
-        return False
+        self.straight(diag)
+        self.inverse(diag)
+        self.straight(bdiag)
+        self.inverse(bdiag)
 
-    def findHorizontal(self):
-        wordTrack = 0
-        for row in self.table():
-            for letter in row:
-                if letter == a.checker(wordTrack, letter):
-                    wordTrack += 1
-                else:
-                    wordTrack = 0
 
 a = Puzzle()
+a.searchHorizontal()
+a.searchVertical()
+a.searchDiagonal()
+print(a)
