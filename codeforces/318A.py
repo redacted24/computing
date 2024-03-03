65ed2429ae14d2c874ca083d9b0fe034027654dd
redacted24 <huangq4@outlook.com>
2024-03-02 22:04:43 -0500

    solved 318A

--- codeforces/318A.py
+++ codeforces/318A.py
@@ -1,1 +1,21 @@
 n,k = [int(x) for x in input().split()]
+
+def solver():
+    def mid(k,n):
+        if n % 2 == 0:
+            if k > n//2:
+                return [True, k-n//2]
+            return [False,k]
+        else:
+            if k > (n//2)+1:
+                return [True, k-(n//2)-1]
+            return [False,k]
+        
+    a = mid(k,n)
+
+    if a[0]:
+        return a[1]*2
+    else:
+        return a[1]*2-1
+
+print(solver())
